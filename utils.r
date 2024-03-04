1a84e8783b90482a78bcf08dc16bb890cc9e98eb
Ali Tasbas <alininkifa45@gmail.com>
2024-03-04 16:05:23 +0300

    updating functions

--- utils.r
+++ utils.r
@@ -1,3 +1,23 @@
-find_z_score <- function(area, mu = 0, sd = 1, lower.tail = T) {
-  z <- qnorm(area, mu = mu, sd = sd, lower.tail = lower.tail)
+# functions
+
+prob_to_z_score <- function(area, mu = 0, sd = 1, lower.tail = T) {
+  z <- qnorm(area, mean = mu, sd = sd, lower.tail = lower.tail)
+  return(z)
+}
+
+find_prob <- function(z = inf, mu = 0, sd = 1, lower.bound = F,  lower.tail = T) {
+  if (lower.bound) {
+    area <- pnorm(q = z, mean = mu, sd = sd) - pnorm(q = lower.bound, mean = mu, sd = sd)
+    # ggplot2
+  } else if (lower.tail == F) {
+    area <- 1 - pnorm(q = z, mean = mu, sd = sd)
+  } else {
+    area <- pnorm(q = z, mean = mu, sd = sd)
+  }
+  return(area)
+}
+
+z_score <- function(x, mu = 0, sd = 1) {
+  z <- (x - mu) / sd
+  return(z)
 }
